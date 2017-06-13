c8d23a2f2b3195afde5f77319201e33fb84ef55d
David Kishonti <david.kishonti@betssongroup.com>
2017-06-13 10:25:32 +0200

    Minor refactor in dataservice

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,5 @@
-import { Injectable } from '@angular/core';
-
-export abstract class DataService{
-    public abstract SaveItem(item:any) :void;
-    public abstract GetItem<T>(key:string) : T;
-    public abstract RemoveItem<T>(key:string) : void;
+export abstract class DataService {
+    public abstract SaveItem(item: any): void;
+    public abstract GetItem<T>(key: string): T;
+    public abstract RemoveItem(key: string): void;
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -4,18 +4,18 @@ import { DataService as DataServiceBase } from '../data.service'
 @Injectable()
 export class DataService extends DataServiceBase {
 
-    private static storageKey: string = "st.storage"
+    private static storageKey = 'st.storage';
 
     public SaveItem(item: any): void {
         localStorage.setItem(DataService.storageKey, JSON.stringify(item));
     }
-    
+
     public GetItem<T>(key: string): T {
-        let item = localStorage.getItem(key);
+        const item = localStorage.getItem(key);
         return JSON.parse(item) as T;
     }
 
-    public RemoveItem<T>(key: string): void {
+    public RemoveItem(key: string): void {
         localStorage.removeItem(key);
     }
 }
