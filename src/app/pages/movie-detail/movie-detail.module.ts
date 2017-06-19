19e5abec8d076176b6deaaca9b64954503025378
dalo02 <daniel.lorincz@betssongroup.com>
2017-06-19 16:55:25 +0200

    Movie detail routing renamed to 'movie/:id'

--- src/app/pages/movie-detail/movie-detail.module.ts
+++ src/app/pages/movie-detail/movie-detail.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
-import { MovieDetailRoutingModule } from './movie-detail-routing.module';
 import { MovieDetailComponent } from './movie-detail.component';
 
 @NgModule({
-    imports: [MovieDetailRoutingModule, MaterialModule, CommonModule ],
+    imports: [ MaterialModule, CommonModule ],
     exports: [],
     declarations: [MovieDetailComponent],
     providers: [],
